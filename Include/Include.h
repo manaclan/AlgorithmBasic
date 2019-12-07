dd33e8e2016ee726d5ac4a988b1d553fdac13f98
manaclan <lucianvinh@gmail.com>
2019-12-07 10:06:56 +0700

    change language to python

--- Include/Include.h
+++
@@ -1,9 +0,0 @@
-#include <math.h>
-#include<iostream>
-#include<string>
-#include<vector>
-using namespace std;
-
-vector<int> SelectionSort(vector<int> Array, int N);
-vector<int> InsertionSort(vector<int> Array, int N);
-vector<int> InterchangeSort(vector<int> Array, int N);
--- README.md
+++
@@ -1,4 +0,0 @@
-# AlgorithmBasic
-A general revision of algorithm that i learned in university time
-
-
